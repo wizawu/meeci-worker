bbc77cb484b0c8028c1d57d40ce203a18b0eba34
wizawu <wizawu@gmail.com>
2014-07-20 18:22:57 +0800

    change minbase apt-get default options

--- minbase.sh
+++ minbase.sh
@@ -11,10 +11,10 @@ DIR='./container'
 mkdir -p $DIR
 
 debootstrap --arch=amd64 --variant=minbase jessie $DIR http://mirrors.163.com/debian
-systemd-nspawn -D $DIR bash -c 'apt-get update; apt-get install -y --fix-missing procps; apt-get clean'
+systemd-nspawn -D $DIR bash -c 'set -e; echo "export DEBIAN_FRONTEND=noninteractive" >> /etc/profile; echo -e "APT::Get::Assume-Yes \"true\";\nAPT::Get::force-yes \"true\";\nquiet 1;" >> /etc/apt/apt.conf.d/11forceyes; apt-get update; apt-get install --fix-missing procps; apt-get clean'
 
 TAR='meeci-minbase.tgz'
-tar zcf $TAR $DIR
+tar -C $DIR -zcf $TAR .
 rm -rf $DIR
 chmod 644 $TAR
 
